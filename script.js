2294ee987403bd298ee3d1efa2ee49210c634962
Subhom <baruahsubhom@gmail.com>
2025-10-21 19:49:49 +0530

    Success

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
+function attendancePercentage(totalHoursTaken, totalHoursAttended) {
+  let percentage = totalHoursAttended/totalHoursTaken*100;
+  return percentage;
+}
+
+function bunkableHours(totalHours, totalHoursTaken, totalHoursAttended) {
+
+  let hoursNotAttended = totalHoursTaken-totalHoursAttended;
+  let totalBunkable = totalHours * 0.25;
+
+  let bunkableHours = totalBunkable - hoursNotAttended;
+  return bunkableHours;
+
+}
+
 function needHours(totalHours, totalHoursAttended, totalTakenHours) {
     let finalPercentage = 0
     let hoursNeeded = 0
@@ -8,3 +23,15 @@ function needHours(totalHours, totalHoursAttended, totalTakenHours) {
     }
     return hoursNeeded
 }
+
+function displayP(percentage) {
+  document.getElementById("displayP").innerText = percentage;
+}
+
+let totalHours = document.getElementById("totalHours").value;
+let totalHoursTaken = document.getElementById("totalHoursTaken").value;
+let totalHoursAttended = document.getElementById("totalHoursAttended").value;
+
+let bunkableHours = bunkableHours(totalHours, totalHoursTaken, totalHoursAttended);
+let attendancePercentage = attendancePercentage(totalHoursTaken, totalHoursAttended);
+let needHours = needHours(totalHours, totalHoursAttended, totalHoursTaken);
