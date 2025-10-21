bfc9bfe4dc2e59edbfd06c281a2e5f822225a00d
Subhom <baruahsubhom@gmail.com>
2025-10-21 19:03:19 +0530

    Add two functions

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
 function attendancePercentage(totalHoursTaken, totalHoursAttended) {
-  let percentage = totalHoursTaken/totalHoursAttended;
+  let percentage = totalHoursAttended/totalHoursTaken*100;
   return percentage;
 }
 
-function bunkableHours() {
+function bunkableHours(totalHours, totalHoursTaken, totalHoursAttended) {
+
+  let hoursNotAttended = totalHoursTaken-totalHoursAttended;
+  let totalBunkable = totalHours * 0.25;
+
+  let bunkableHours = totalBunkable - hoursNotAttended;
+  return bunkableHours;
 
 }
 
-function needHours(totalHours, totalHoursAttended, totalTakenHours) {
-  
+function needHours(totalHours, totalHoursTaken, totalHoursAttended) {
+
 }
